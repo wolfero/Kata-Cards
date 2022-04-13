a6d5f0068be0598fee207c30454bebfcd9257035
Wolfero <ferorock44@gmail.com>
2022-04-13 14:44:27 +0100

    🟢 test: return_message_when_cards_are_not_given

---
+++ src/main/java/CardsGame.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class CardsGame {
+    public String startOut(List<String> player1Card, List<String> player2Card) {
+        throw new IllegalArgumentException("You have to provide correct format");
+    }
+
+}
