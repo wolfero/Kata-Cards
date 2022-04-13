8bc0ab4ad49e3b7e592c4be6989cd1c0398c64af
Wolfero <ferorock44@gmail.com>
2022-04-13 15:21:54 +0100

    ✨ new use cases

--- src/test/java/CardsGameShould.java
+++ src/test/java/CardsGameShould.java
@@ -8,15 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardsGameShould {
     /* TODO
-    * * CARD RANK -> ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
-    * * DONE --> ([],[]) -> throw exception "You have to provide correct format"
-    * * DONE --> (["",""],["",""]) -> throw exception "You have to provide a player card"
-    * * (["K",""],["A","J"]) -> throw exception "You have to provide a player card"
-    * * (["z",""],["5",""]) -> throw exception "You have to provide a valid card"
-    * * (["3",""],["A",""]) -> return "Player 2 win 1 to 0"
-    * * (["K","9"],["J","5"]) -> return "Player 1 win 2 to 0"
-    * * (["K","9"],["J","A"]) -> return "Players tied"
-    * */
+     * * CARD RANK -> ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+     * * DONE --> ([],[]) -> throw exception "You have to provide correct format"
+     * *  --> ([""],["",""]) -> throw exception "You have to provide correct format"
+     * * DONE --> (["",""],["",""]) -> throw exception "You have to provide a player card"
+     * *  --> (["K",""],["A","J"]) -> throw exception "You have to provide a player card"
+     * * (["z",""],["5",""]) -> throw exception "You have to provide a valid card"
+     * * (["3",""],["A",""]) -> return "Player 2 win 1 to 0"
+     * * (["K","9"],["J","5"]) -> return "Player 1 win 2 to 0"
+     * * (["K","9"],["J","A"]) -> return "Players tied"
+     * */
 
     @Test
     void return_message_when_hands_are_not_given() {
@@ -31,7 +32,7 @@ public class CardsGameShould {
     void return_message_when_cards_are_not_given() {
         var cardsGame = new CardsGame();
 
-        var exception = assertThrows(IllegalArgumentException.class, () -> cardsGame.startOut(List.of("",""), List.of("","")));
+        var exception = assertThrows(IllegalArgumentException.class, () -> cardsGame.startOut(List.of("", ""), List.of("", "")));
 
         assertThat(exception.getMessage()).isEqualTo("You have to provide a player card");
     }
