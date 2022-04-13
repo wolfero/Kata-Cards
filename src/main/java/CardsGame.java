db515d5b87c66a14077adc9a9f8e7f1cef8d7c8e
Wolfero <ferorock44@gmail.com>
2022-04-13 14:44:51 +0100

    ♻ Refactor

--- src/main/java/CardsGame.java
+++ src/main/java/CardsGame.java
@@ -2,7 +2,13 @@ import java.util.List;
 
 public class CardsGame {
     public String startOut(List<String> player1Card, List<String> player2Card) {
-        throw new IllegalArgumentException("You have to provide correct format");
+        isValid(player1Card, player2Card);
+        return "";
     }
 
+    private void isValid(List<String> player1Card, List<String> player2Card) {
+        if (player1Card.isEmpty() || player2Card.isEmpty()) {
+            throw new IllegalArgumentException("You have to provide correct format");
+        }
+    }
 }
