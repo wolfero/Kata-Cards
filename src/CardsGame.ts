5305b32b1a9f530eb143cbaa09dcf2424e469a32
Wolfero <ferorock44@gmail.com>
2022-04-14 08:41:11 +0100

    ♻ Refactor

--- src/CardsGame.ts
+++ src/CardsGame.ts
@@ -1,6 +1,19 @@
 export class CardsGame {
   public play(player1Hand:string[],player2Hand:string[]): string {
-    throw new Error("You have to provide correct format");
+    this.isValid(player1Hand,player2Hand);
+
     return "";
+  }
+
+  private isValid(player1Hand:string[],player2Hand:string[]): void {
+    const hadEmptyHand = this.hadEmptyHands(player1Hand, player2Hand);
+    if(hadEmptyHand){
+      throw new Error("You have to provide correct format");
+    }
+
+  }
+
+  private hadEmptyHands(player1Hand: string[], player2Hand: string[]) {
+    return player1Hand.length != 2 && player2Hand.length != 2;
   }
 }
