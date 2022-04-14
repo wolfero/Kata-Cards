a2f7fd5a88cc82820c7a15738b0eba5126d84c2b
Wolfero <ferorock44@gmail.com>
2022-04-14 09:06:57 +0100

    🟢 test: Returns message when player cards are invalid

--- src/CardsGame.ts
+++ src/CardsGame.ts
@@ -1,7 +1,9 @@
 export class CardsGame {
+  private readonly CARDS_VALUES:string[] = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+
   public play(player1Hand:string[],player2Hand:string[]): string {
     this.isValid(player1Hand,player2Hand);
-
+    
     return "";
   }
 
@@ -10,7 +12,7 @@ export class CardsGame {
     if(hadEmptyHand){
       throw new Error("You have to provide correct format");
     }
-
+    throw new Error("You have to provide valid cards");
   }
 
   private hadEmptyHands(player1Hand: string[], player2Hand: string[]) {
