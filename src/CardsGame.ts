b4135cbcc6e46367cb700b3564c807e6369f75a2
Wolfero <ferorock44@gmail.com>
2022-04-14 09:22:26 +0100

    ♻ Refactor

--- src/CardsGame.ts
+++ src/CardsGame.ts
@@ -2,20 +2,29 @@ export class CardsGame {
   private readonly CARDS_VALUES:string[] = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
 
   public play(player1Hand:string[],player2Hand:string[]): string {
-    this.isValid(player1Hand,player2Hand);
+    this.validate(player1Hand,player2Hand);
     
     return "";
   }
 
-  private isValid(player1Hand:string[],player2Hand:string[]): void {
+  private validate(player1Hand:string[],player2Hand:string[]): void {
     const hadEmptyHand = this.hadEmptyHands(player1Hand, player2Hand);
     if(hadEmptyHand){
       throw new Error("You have to provide correct format");
     }
-    throw new Error("You have to provide valid cards");
+    
+    const shouldValidCard=this.isValidCard(player1Hand)&& this.isValidCard(player2Hand);
+    if(!shouldValidCard){
+      throw new Error("You have to provide valid cards");
+    }
   }
 
-  private hadEmptyHands(player1Hand: string[], player2Hand: string[]) {
+  private isValidCard([card1, card2]: string[]):boolean {
+    if (this.CARDS_VALUES.includes(card1) && this.CARDS_VALUES.includes(card2)) return true;
+    return false;
+  }
+
+  private hadEmptyHands(player1Hand: string[], player2Hand: string[]):boolean {
     return player1Hand.length != 2 && player2Hand.length != 2;
   }
 }
