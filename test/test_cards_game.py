d27c7e47d855295298f02e5bd94d8dabbe85f25b
Wolfero <ferorock44@gmail.com>
2022-04-15 19:14:20 +0100

    🔴 test: test_returns_message_when_player_hands_are_empty

--- test/test_cards_game.py
+++ test/test_cards_game.py
@@ -1,7 +1,15 @@
-from assertpy import assert_that
 from unittest import TestCase
+from assertpy import assert_that
 
-from src.cards_game import WordWrapper
+from src.cards_game import CardGame
 
 
 class TestCardsGame(TestCase):
+    def test_returns_message_when_player_hands_are_empty(self):
+        cardGame = CardGame()
+
+        player1 = ["", ""]
+        player2 = ["", ""]
+
+        assert_that(cardGame.play(player1, player2)).is_equal_to(
+            'You have to provide correct format')
