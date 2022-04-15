b8731aa58b29d26fc44c42b399504dee12d339e8
Wolfero <ferorock44@gmail.com>
2022-04-15 19:15:05 +0100

    🟢 test: test_returns_message_when_player_hands_are_empty

--- src/cards_game.py
+++ src/cards_game.py
@@ -1,2 +1,5 @@
-class WordWrapper:
-    
+class CardGame:
+    CARDS_VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    def play(self,player1Hand: list, player2Hand: list) -> str:
+        return "You have to provide correct format"
