e4af5f39b857c70ebcd505a05a5d479699d3c86f
Wolfero <ferorock44@gmail.com>
2022-04-15 19:59:52 +0100

    ♻ Refactor

--- src/cards_game.py
+++ src/cards_game.py
@@ -1,22 +1,22 @@
 class CardGame:
     CARDS_VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-    def play(self,player1Hand: list, player2Hand: list) -> str:
+    def play(self, player1Hand: list, player2Hand: list) -> str:
         self.isValid(player1Hand, player2Hand)
+
         return ""
 
     def isValid(self, player1Hand: list, player2Hand: list):
         self.hadEmptyHand(player1Hand, player2Hand)
-        
+
+        self.hadValidCards(player1Hand, player2Hand)
+
+    def hadEmptyHand(self, player1Hand: list, player2Hand: list):
+        if len(player1Hand) != 2 or len(player2Hand) != 2:
+            raise ValueError("You have to provide correct format")
+
+    def hadValidCards(self, player1Hand: list, player2Hand: list):
         self.CARDS_VALUES.index(player1Hand.__getitem__(0))
         self.CARDS_VALUES.index(player1Hand.__getitem__(1))
         self.CARDS_VALUES.index(player2Hand.__getitem__(0))
         self.CARDS_VALUES.index(player2Hand.__getitem__(1))
-        
-        
-        
-        
-
-    def hadEmptyHand(self, player1Hand: list, player2Hand: list):    
-        if len(player1Hand)!=2 or len(player2Hand)!=2:
-            raise ValueError("You have to provide correct format")
