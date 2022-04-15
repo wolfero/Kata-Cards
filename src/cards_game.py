bb32604f9f5f74d15f02523b63dd33bd7d43e821
Wolfero <ferorock44@gmail.com>
2022-04-15 20:24:10 +0100

    ♻ Refactor

--- src/cards_game.py
+++ src/cards_game.py
@@ -4,19 +4,26 @@ class CardGame:
     def play(self, player1Hand: list, player2Hand: list) -> str:
         self.isValid(player1Hand, player2Hand)
 
-        return "Player 1 wins"
+        return self.whoWins(player1Hand, player2Hand)
 
     def isValid(self, player1Hand: list, player2Hand: list):
         self.hadEmptyHand(player1Hand, player2Hand)
 
-        self.hadValidCards(player1Hand, player2Hand)
+        # self.hadValidCards(player1Hand, player2Hand)
+
+    def whoWins(self, player1Hand: list, player2Hand: list) -> str:
+        player1Value = self.getCardsValue(player1Hand.__getitem__(0), player1Hand.__getitem__(1))
+        player2Value = self.getCardsValue(player2Hand.__getitem__(0), player2Hand.__getitem__(1))
+        
+        winner=player1Value>player2Value
+        if winner:
+            return "Player 1 wins"
+        else:
+            return "Player 2 wins"
+
+    def getCardsValue(self, leftCard: str, rightCard) -> int:
+        return self.CARDS_VALUES.index(leftCard.upper()) + self.CARDS_VALUES.index(rightCard.upper())
 
     def hadEmptyHand(self, player1Hand: list, player2Hand: list):
         if len(player1Hand) != 2 or len(player2Hand) != 2:
             raise ValueError("You have to provide correct format")
-
-    def hadValidCards(self, player1Hand: list, player2Hand: list):
-        self.CARDS_VALUES.index(player1Hand.__getitem__(0))
-        self.CARDS_VALUES.index(player1Hand.__getitem__(1))
-        self.CARDS_VALUES.index(player2Hand.__getitem__(0))
-        self.CARDS_VALUES.index(player2Hand.__getitem__(1))
