76d212a0d16ee1ee39f3257afe39652324ed4ac3
Wolfero <ferorock44@gmail.com>
2022-04-15 20:26:59 +0100

    🟢 test: test_returns_draw_if_nobody_is_winner

--- src/cards_game.py
+++ src/cards_game.py
@@ -12,14 +12,16 @@ class CardGame:
         # self.hadValidCards(player1Hand, player2Hand)
 
     def whoWins(self, player1Hand: list, player2Hand: list) -> str:
-        player1Value = self.getCardsValue(player1Hand.__getitem__(0), player1Hand.__getitem__(1))
-        player2Value = self.getCardsValue(player2Hand.__getitem__(0), player2Hand.__getitem__(1))
-        
-        winner=player1Value>player2Value
-        if winner:
+        player1Value = self.getCardsValue(
+            player1Hand.__getitem__(0), player1Hand.__getitem__(1))
+        player2Value = self.getCardsValue(
+            player2Hand.__getitem__(0), player2Hand.__getitem__(1))
+
+        if player1Value > player2Value:
             return "Player 1 wins"
-        else:
+        if player1Value < player2Value:
             return "Player 2 wins"
+        return "Draw"
 
     def getCardsValue(self, leftCard: str, rightCard) -> int:
         return self.CARDS_VALUES.index(leftCard.upper()) + self.CARDS_VALUES.index(rightCard.upper())
--- test/test_cards_game.py
+++ test/test_cards_game.py
@@ -9,18 +9,19 @@ class TestCardsGame(TestCase):
         cardGame = CardGame()
 
         assert_that(cardGame.play).raises(ValueError).when_called_with([], [])
-    
+
     def test_returns_message_when_player_cards_are_invalid(self):
         cardGame = CardGame()
 
-        assert_that(cardGame.play).raises(ValueError).when_called_with(["z", "f"], ["f", "f"])
-        
+        assert_that(cardGame.play).raises(
+            ValueError).when_called_with(["z", "f"], ["f", "f"])
+
     def test_returns_winner(self):
         cardGame = CardGame()
-        
+
         assert_that(cardGame.play(["K", "J"], ["K", "2"])).is_equal_to("Player 1 wins")
-    
+
     def test_returns_draw_if_nobody_is_winner(self):
         cardGame = CardGame()
-        
+
         assert_that(cardGame.play(["Q", "A"], ["A", "Q"])).is_equal_to("Draw")
